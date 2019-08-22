142859e69b1c7b9dbed894e5ae8bac1cf210ac22
Diego Vera <omar.vera@gmail.com>
2019-08-22 17:15:11 +0200

    Using MongoDB mappers

--- moviesApp/moviesApp/Model/Movie.cs
+++ moviesApp/moviesApp/Model/Movie.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+
 namespace moviesApp.Model
 {
     public class Movie
     {
+        [BsonId]
+        [JsonIgnore]
+        public ObjectId Id { get; set; }
+        public string MovieId
+        {
+            get { return Id.ToString(); }
+            set { Id = ObjectId.Parse(value); }
+        }
         public string Title { get; set; }
         public string Director { get; set; }
         public string Actors { get; set; }
         public string Image { get; set; }
         public int Year { get; set; }
+        [BsonConstructor]
+        public Movie(string title, string director, string actors, string image, int year)
+        {
+            Title = title;
+            Director = director;
+            Actors = actors;
+            Image = image;
+            Year = year;
+        }
     }
 }
--- moviesApp/moviesApp/Repository/MoviesRepository.cs
+++ moviesApp/moviesApp/Repository/MoviesRepository.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
 using moviesApp.Controllers;
 using moviesApp.Model;
@@ -11,36 +11,37 @@ namespace moviesApp.Repository
     {
         MongoClient client;
         IMongoDatabase database;
-        IMongoCollection<BsonDocument> collection;
+        IMongoCollection<Movie> collection;
         public MoviesRepository()
         {
-            // or use a connection string
+            BsonClassMap.RegisterClassMap<Movie>(cm =>
+            {
+                cm.AutoMap();
+                
+                cm.MapMember(c => c.Title).SetElementName("title");
+                cm.MapMember(c => c.Director).SetElementName("director");
+                cm.MapMember(c => c.Image).SetElementName("image");
+                cm.MapMember(c => c.Actors).SetElementName("actors");
+                cm.MapMember(c => c.Year).SetElementName("year");
+                
+            });
+
+            // Use the connection string
             client = new MongoClient("mongodb+srv://service:rSSCDZTIZzwPRsnEHGvY@cluster0-iv5oy.mongodb.net");
             database = client.GetDatabase("movie");
 
-            collection = database.GetCollection<BsonDocument>("movie");
+            collection = database.GetCollection<Movie>("movie");
         }
 
         public async Task<IEnumerable<Movie>> ListMovies()
         {
-            var documents = await collection.Find(new BsonDocument()).ToListAsync();
-            List<Movie> movies = new List<Movie>();
+            var documents = await collection.AsQueryable().ToListAsync();
+            var movies = new List<Movie>();
 
-            foreach (BsonDocument doc in documents)
+            foreach (Movie movie in documents)
             {
-
-                Movie movie = new Movie
-                {
-                    Title = doc.GetString("title"),
-                    Actors = doc.GetString("actors"),
-                    Director = doc.GetString("director"),
-                    Image = doc.GetString("image"),
-                    Year = doc.GetInt("year")
-                };
-
                 movies.Add(movie);
             }
-
 
             return movies;
 
